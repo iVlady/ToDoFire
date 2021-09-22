a6db58c845003912a6718fe1207c265b6ada0302
iVlady <vinogrotskiy97@gmail.com>
2021-09-22 22:26:50 +0300

    Created an interface

--- ToDoFire/ViewController.swift
+++ ToDoFire/ViewController.swift
@@ -9,11 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toDoFireLabel: UILabel!
+    @IBOutlet weak var userDoesNotExistLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        toDoFireLabelSetup()
+        userDoesNotExistLabelSetup()
+        emailTextFieldSetup()
+        passwordTextFieldSetup()
+        logInButtonSetup()
+        registerButtonSetup()
     }
+    
+    
+    @IBAction func logInButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+    }
+    
 
+}
 
+extension ViewController{
+    
+    func toDoFireLabelSetup() {
+        toDoFireLabel.text = "To Do FIRE"
+        toDoFireLabel.textColor = .white
+        toDoFireLabel.font = UIFont(name: "Rockwell", size: 40)
+        toDoFireLabel.textAlignment = .center
+    }
+    
+    func userDoesNotExistLabelSetup() {
+        userDoesNotExistLabel.text = "User does not exist"
+        userDoesNotExistLabel.textColor = .red
+        userDoesNotExistLabel.font = UIFont(name: "Rockwell", size: 18)
+        userDoesNotExistLabel.textAlignment = .center
+    }
+    
+    func emailTextFieldSetup() {
+        emailTextField.placeholder = "Email"
+        emailTextField.textAlignment = .center
+        emailTextField.font = UIFont(name: "Rockwell", size: 17)
+    }
+    
+    func passwordTextFieldSetup() {
+        passwordTextField.placeholder = "Password"
+        passwordTextField.textAlignment = .center
+        passwordTextField.font = UIFont(name: "Rockwell", size: 17)
+        passwordTextField.isSecureTextEntry = true
+    }
+    
+    func logInButtonSetup() {
+        logInButton.titleLabel?.text = "Log In"
+        logInButton.titleLabel?.textColor = .white
+        logInButton.titleLabel?.font = UIFont(name: "Rockwell", size: 23)
+    }
+    
+    func registerButtonSetup() {
+        registerButton.titleLabel?.text = "Register"
+        registerButton.titleLabel?.textColor = .white
+        registerButton.titleLabel?.font = UIFont(name: "Rockwell", size: 23)
+    }
 }
 
